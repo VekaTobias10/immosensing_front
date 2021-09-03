567b33a317a5c4a058cd0b01a538c5848f423261
VekaTobias10 <vekatobias@outlook.es>
2021-09-03 09:00:14 +0200

    changes landing page, services and doing users

--- immosensing/src/components/components-landingpage/header-landing/style.js
+++ immosensing/src/components/components-landingpage/header-landing/style.js
@@ -1,12 +1,98 @@
 import { makeStyles } from '@material-ui/core/styles';
+import headerPhoto from '../../../assets/img/landingpage-img/pexels-bcn.jpg';
 
 
 export const useStyles = makeStyles((theme) => ({
+    //navbar
+    mainNavBar: {
+        // backgroundImage: `url(${headerPhoto})`,
+        background: `linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${headerPhoto})`,
+        position: 'relative',
+        paddingBottom:'5rem',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        left: '0',
+        top: '0',
+        width: '100%',
+        height: '100%',
+    },
+    containerNavBar: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: '1.5rem',
+        justifyContent: 'space-between',
+        color: 'white',
+        position: 'relative',
+        textDecoration: 'underline',
+        letterSpacing: '0.2rem',
+        fontWeight: 'bold',
+        width: '80%',
+        margin: 'auto',
+    },
 
-    imgHeaderLanding:{
-        margin:'auto',
-        border: '20px solid #0FCDB2',
-        borderRadius: '20px'
+    logoLanding: {
+        width: '13%',
+    },
+
+    containerList: {
+        listStyleType: 'none',
+        display: 'flex'
+    },
+    listItem: {
+        margin: '1rem'
+    },
+    button_lang: {
+        color: 'white',
+        margin: '0.3rem',
+    },
+    button_lang2: {
+        color: '#0FCDB2',
+        margin: '0.3rem',
+    },
+    //Header
+    mainheader: {
+        width: '100%',
+        margin: 'auto'
+    },
+
+    welcomeHeader: {
+        width: '40%',
+        border: '8px solid #0FCDB2',
+        margin: '2rem auto 1.5rem',
+        color: 'grey',
+        padding: '1rem',
+        textAlign: 'left',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+    },
+
+    titleImmo: {
+        fontSize: '3rem',
+        color: 'white',
+    },
+    subtitleHeader: {
+        marginTop: '1rem',
+        marginBottom: '1rem',
+        fontSize: '1.5rem',
+        color: 'white',
+    },
+    containerButton: {
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+    },
+    buttonRegisterContainer: {
+        textAlign: 'center',
+    },
+    buttonRegister: {
+        color: '#0FCDB2',
+        fontSize: '1.2rem'
+    },
+    btnAccess: {
+        backgroundColor: '#0FCDB2',
+        color: 'white',
+        width: '8rem',
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     }
-
 }));
